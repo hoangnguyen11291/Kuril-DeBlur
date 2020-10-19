625d861f2faa97dd3dc43104212c796d1b8c6119
HoangNguyen <68989921+hoangnguyen11291@users.noreply.github.com>
2020-10-19 12:33:40 +0700

    Add files via upload

---
+++ models/models.py
@@ -0,0 +1,40 @@
+import numpy as np
+import torch.nn as nn
+import torch
+from skimage.measure import compare_ssim as SSIM
+
+from util.metrics import PSNR
+
+
+class DeblurModel(nn.Module):
+    def __init__(self):
+        super(DeblurModel, self).__init__()
+
+    def get_input(self, data):
+        img = data['a']
+        inputs = img
+        img = data['b']
+        targets = img
+        return self.array_to_batch(inputs), self.array_to_batch(targets)
+
+    def array_to_batch(self, x):
+        x = np.expand_dims(x, 0)
+        return torch.from_numpy(x).cuda()
+
+    def tensor2im(self, image_tensor, imtype=np.uint8):
+        image_numpy = image_tensor[0].cpu().float().numpy()
+        image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
+        return image_numpy.astype(imtype)
+
+    def get_images_and_metrics(self, inp, output, target) -> (float, float, np.ndarray):
+        inp = self.tensor2im(inp)
+        fake = self.tensor2im(output.data)
+        real = self.tensor2im(target.data)
+        psnr = PSNR(fake, real)
+        ssim = SSIM(fake, real, multichannel=True)
+        vis_img = np.hstack((inp, fake, real))
+        return psnr, ssim, vis_img
+
+
+def get_model(model_config):
+    return DeblurModel()
